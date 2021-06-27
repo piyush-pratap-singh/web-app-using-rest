3e78610b60bba837325d8fb50f76dcd56304c67e
Piyush Pratap Singh <85784053+piyush-pratap-singh@users.noreply.github.com>
2021-06-27 21:40:49 +0530

    Add files via upload
    Added eureka discovery client

---
+++ eureka-discovery-service/src/main/java/com/eureka/discovery/service/app/eurekadiscoveryservice/EurekaDiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package com.eureka.discovery.service.app.eurekadiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ eureka-discovery-service/src/test/java/com/eureka/discovery/service/app/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.discovery.service.app.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
